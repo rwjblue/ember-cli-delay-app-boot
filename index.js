3b84d07c185bb51754814d51bc6904d8208b5b83
Robert Jackson <robert.w.jackson@me.com>
2015-03-31 14:01:29 -0400

    Do not delay boot in production builds.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
   name: 'ember-cli-delay-app-boot',
 
   contentFor: function(type, config) {
-    if (type === 'app-boot') {
+    if (type === 'app-boot' && this.app.env !== 'production') {
       return 'setTimeout(function() {\n' +
                'require("' + config.modulePrefix + '/app")["default"]' +
                  '.create(' + calculateAppConfig(config) + ');\n' +
