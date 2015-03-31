b015be231e85d121a04f52bf4d0463e08b055afd
Robert Jackson <robert.w.jackson@me.com>
2015-03-31 12:10:16 -0400

    Make it so.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+function calculateAppConfig(config) {
+  return JSON.stringify(config.APP || {});
+}
+
 module.exports = {
-  name: 'ember-cli-delay-app-boot'
+  name: 'ember-cli-delay-app-boot',
+
+  contentFor: function(type, config) {
+    if (type === 'app-boot') {
+      return 'setTimeout(function() {\n' +
+               'require("' + config.modulePrefix + '/app")["default"]' +
+                 '.create(' + calculateAppConfig(config) + ');\n' +
+              '}, 250);';
+    }
+  },
+
+  included: function() {
+    this.app.options.autoRun = false;
+  }
 };
